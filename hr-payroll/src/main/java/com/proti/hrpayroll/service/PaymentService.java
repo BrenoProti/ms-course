34480311058a0e327a57565f531d1adec75c6e0d
Breno <moreirasgbr@gmail.com>
2022-06-30 21:44:16 -0300

    FeignClient implementation

--- hr-payroll/src/main/java/com/proti/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/proti/hrpayroll/service/PaymentService.java
@@ -1,29 +1,20 @@
 package com.proti.hrpayroll.service;
 
+import com.proti.hrpayroll.client.WorkerClient;
 import com.proti.hrpayroll.model.entity.Payment;
 import com.proti.hrpayroll.model.entity.Worker;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
 
-    private final RestTemplate restTemplate;
+    private final WorkerClient workerClient;
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
 
     public Payment getPayment(Long workerId, Integer days) {
-        Map<String, String> uriVariable = new HashMap<>();
-        uriVariable.put("id", workerId.toString());
-        String url = workerHost.concat("/workers/{id}");
-        Worker worker = restTemplate.getForObject(url, Worker.class, uriVariable);
+        Worker worker = this.workerClient.findById(workerId).getBody();
         assert worker != null;
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
