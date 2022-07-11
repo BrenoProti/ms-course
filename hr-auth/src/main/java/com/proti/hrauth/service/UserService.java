451523fa85d91caee07888f85126f3e24bc2e16b
Breno <moreirasgbr@gmail.com>
2022-07-11 11:25:18 -0300

    Configure authorization

--- hr-auth/src/main/java/com/proti/hrauth/service/UserService.java
+++ hr-auth/src/main/java/com/proti/hrauth/service/UserService.java
@@ -4,12 +4,15 @@ import com.proti.hrauth.client.UserClient;
 import com.proti.hrauth.model.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserClient client;
 
@@ -20,5 +23,16 @@ public class UserService {
             log.error("Erro ao tentar comunicar com a API de usário: ".concat(e.getMessage()));
             throw new RuntimeException(e.getMessage());
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = client.findByEmail(username).getBody();
+        if (user == null) {
+            log.error("Email not found: " + username);
+            throw new UsernameNotFoundException("Email not found");
+        }
+        log.info("Email found: " + username);
+        return user;
     }
 }
